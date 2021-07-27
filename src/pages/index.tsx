98b560858f1cae86b845074bcbc680812005b2f9
Lucaslc <lucaslc987654@gmail.com>
2021-07-27 19:56:28 -0300

    SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import styles from './home.module.scss';
 
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
 
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async() => {
+export const getStaticProps: GetStaticProps = async() => {
     const price = await stripe.prices.retrieve('price_1JHfemIvWjLT4Y1QrCHZONGN')
 
     const product = {
@@ -52,6 +52,7 @@ export const getServerSideProps: GetServerSideProps = async() => {
     return { 
         props: {
             product,
-        }
+        },
+        revalidate: 60 * 60 * 24,
     }
 } 
